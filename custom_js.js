07afe462d634805a9e17b6875cd1534a703249d7
Jia Ming <jma8774@bths.edu>
2020-11-25 00:31:08 -0500

    finished jett page

---
+++ custom_js.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  $("#a1-btn").click(function(){
+    $("#a1").show();
+    $("#a2").hide();
+    $("#a3").hide();
+    $("#a4").hide();
+    $('#a1-v').trigger('load');
+    $('#a2-v').trigger('pause');
+    $('#a3-v').trigger('pause');
+    $('#a4-v').trigger('pause');
+  });
+});
+
+$(document).ready(function(){
+  $("#a2-btn").click(function(){
+    $("#a1").hide();
+    $("#a2").show();
+    $("#a3").hide();
+    $("#a4").hide();
+    $('#a1-v').trigger('pause'); 
+    $('#a2-v').trigger('load');
+    $('#a3-v').trigger('pause');
+    $('#a4-v').trigger('pause');
+
+  });
+});
+
+$(document).ready(function(){
+  $("#a3-btn").click(function(){
+    $("#a1").hide();
+    $("#a2").hide();
+    $("#a3").show();
+    $("#a4").hide();
+    $('#a1-v').trigger('pause');
+    $('#a2-v').trigger('pause');
+    $('#a3-v').trigger('load');
+    $('#a4-v').trigger('pause');
+  });
+});
+
+$(document).ready(function(){
+  $("#a4-btn").click(function(){
+    $("#a1").hide();
+    $("#a2").hide();
+    $("#a3").hide();
+    $("#a4").show();
+    $('#a1-v').trigger('pause'); 
+    $('#a2-v').trigger('pause');
+    $('#a3-v').trigger('pause');
+    $('#a4-v').trigger('load');
+  });
+});
